4404cb17717d6bce4d99784fe9d39dd8b0875fae
Ashley Kapaso <ashleykapaso@gmail.com>
2023-09-12 17:48:16 +0200

    add file upload config

--- server/index.js
+++ server/index.js
@@ -24,5 +24,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
-// TO-DO
+const upload = multer({ storage });
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
