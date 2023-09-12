c73a821cd1675bf0c042540135d1a61a237a412f
Ashley Kapaso <ashleykapaso@gmail.com>
2023-09-12 17:41:32 +0200

    add configs

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import path from "path";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import multer from "multer"
+import morgan from "morgan";
+import express from "express";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS */
+dotenv.config();
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(morgan("common"));
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+// TO-DO
+
